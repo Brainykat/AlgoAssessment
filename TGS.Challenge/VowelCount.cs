249580afbca6f90ac16b4df20ce0385072b78ba6
Chris <eng.christhuku@gmail.com>
2021-03-31 09:16:58 +0300

    add vowel count linq function

--- TGS.Challenge/VowelCount.cs
+++ TGS.Challenge/VowelCount.cs
@@ -1,22 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TGS.Challenge
 {
-    /*
-        Devise a function that takes a string & returns the number of 
-        vowels (aeiou) in that string.
+  /*
+      Devise a function that takes a string & returns the number of 
+      vowels (aeiou) in that string.
 
-        "Hi there!" = 3
-        "What do you mean?"  = 6
+      "Hi there!" = 3
+      "What do you mean?"  = 6
 
-        There are accompanying unit tests for this exercise, ensure all tests pass & make
-        sure the unit tests are correct too.
-     */
-    public class VowelCount
+      There are accompanying unit tests for this exercise, ensure all tests pass & make
+      sure the unit tests are correct too.
+   */
+  public class VowelCount
+  {
+    public int Count(string value)
     {
-        public int Count(string value)
-        {
-            return -1;
-        }
+      if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException($"Supply the first word {value}");
+      var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+      return value.Count(c => vowels.Contains(c));
     }
+  }
 }
